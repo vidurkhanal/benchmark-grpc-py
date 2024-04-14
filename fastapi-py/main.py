53a1f0c60c45ae9c917fdd1a4d0422126dc402c2
vidurkhanal <vkhanal@caldwell.edu>
2024-04-14 14:33:31 -0400

    chore: added  upload_file

--- fastapi-py/main.py
+++ fastapi-py/main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, UploadFile, File
 
 app = FastAPI()
 
@@ -6,3 +6,10 @@ app = FastAPI()
 @app.get("/ping")
 async def ping():
     return {"message": "pong", "status": "OK"}
+
+
+@app.post("/upload-file")
+async def upload_file(file: UploadFile):
+    for chunk in file.file:
+        print(chunk)
+    return {"message": "upload file", "status": "OK"}
