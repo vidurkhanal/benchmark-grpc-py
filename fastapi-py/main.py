f9c890e7d19b40f7ad75d99a0bcc39579b5e6a8a
vidurkhanal <vkhanal@caldwell.edu>
2024-04-14 21:31:51 -0400

    chore: minor fixes and refactorings

--- fastapi-py/main.py
+++ fastapi-py/main.py
@@ -1,15 +1,27 @@
-from fastapi import FastAPI, UploadFile, File
+import os
+import time
+import uuid
+from fastapi import FastAPI, UploadFile
 
 app = FastAPI()
 
 
 @app.get("/ping")
 async def ping():
-    return {"message": "pong", "status": "OK"}
+    time.sleep(3)
+    return {"message": str(uuid.uuid4()), "status": "OK"}
 
 
 @app.post("/upload-file")
 async def upload_file(file: UploadFile):
+    file_data = b""
+    file_name = str(uuid.uuid4())
     for chunk in file.file:
-        print(chunk)
+        file_data += chunk
+
+    with open(f"received/{file_name}.bin", "wb") as _file:
+        _file.write(file_data)
+
+    os.remove(f"received/{file_name}.bin")
+
     return {"message": "upload file", "status": "OK"}
